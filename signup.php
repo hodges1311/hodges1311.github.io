a577a075ed700764be6fc88b68594efd75797101
RushSetty <rs5yb@virginia.edu>
2018-08-31 12:34:27 -0400

    k

--- signup.php
+++ signup.php
@@ -23,13 +23,6 @@ session_start();
 
 	<body>
 		<?php
-			use PHPMailer\PHPMailer\PHPMailer;
-			use PHPMailer\PHPMailer\Exception;
-			
-			require 'PHPMailer/src/Exception.php';
-			require 'PHPMailer/src/PHPMailer.php';
-			require 'PHPMailer/src/SMTP.php';
-
 			$mp = "";
 			if(isset($_SESSION["redirect"]))
 				$mp = $_SESSION["redirect"];
